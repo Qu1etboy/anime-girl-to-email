f29b630e343e8613999298b84dfcca03ac2c4034
Qu1etboy <nonzagreanthai@gmail.com>
2023-02-09 22:57:26 +0700

    feat(email): sent anime girl to your email

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -14,17 +14,18 @@ export const transporter = nodemailer.createTransport({
 export const mailOptions = (email: string) => ({
   from: process.env.EMAIL,
   to: email,
-  subject: "Purchase successfully",
+  subject: "Here is your anime girl picture.",
   html: `
-  <main style="width: 100%; max-width: 672px; margin: 0 auto;">
-    <h1>Thank you for purchased the product from us</h1>
-    <h2>Your product is being delivery</h2>
-    <div style="border: 1px solid black; border-radius: 0.375rem; padding: 1.25rem;">
-      <img style="width: 100%;" src="https://images.unsplash.com/photo-1580522154071-c6ca47a859ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="macbook pro" />
-      <h3>Macbook Pro</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam facilisis, neque in consectetur pulvinar, tellus ligula accumsan urna, eget ultrices felis diam id odio. Maecenas efficitur rhoncus viverra. Vivamus scelerisque lacus sed ipsum pharetra, id malesuada sapien rhoncus. Sed purus dolor, porttitor nec interdum eget, volutpat efficitur nisl.</p>
-      <button>View Detail</button>    
-    </div>
+  <main>
+    <h1>Thank you for using our service!</h1>
+    <img src="cid:${email}" style="width:720px;"/>
   </main>
   `,
+  attachments: [
+    {
+      filename: "anime.png",
+      path: "https://pic.re/image",
+      cid: email, //same cid value as in the html img src
+    },
+  ],
 });
