9ab684941a40626abbb1a6ff780f47616c4108ed
Qu1etboy <nonzagreanthai@gmail.com>
2023-02-10 15:07:22 +0700

    feat(web): sent email to backend

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,17 +1,24 @@
 import express, { Request, Response } from "express";
 import { sendEmail } from "./services/email";
+import cors from "cors";
 
 export const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 
 app.post("/sendEmail", async (req: Request, res: Response) => {
   const { email } = req.body;
-
+  console.log(email);
   try {
     const info = await sendEmail(email);
 
     res.status(201).send(info.response);
   } catch (err) {
+    console.error(err);
     res
       .status(500)
       .send("There was an error sending an email please try again.");
