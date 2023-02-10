26246657ffc9d362624fdb7ffa44a7dc6dfa46bd
Qu1etboy <nonzagreanthai@gmail.com>
2023-02-11 02:58:33 +0700

    style(web): add more design

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
@@ -36,19 +37,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex flex-col items-center">
-        <h1 className="text-6xl font-extrabold mt-10 mb-5">
+        <h1 className="text-6xl font-extrabold mt-10 mb-5 bg-clip-text bg-center text-black/20 bg-[url('https://e1.pxfuel.com/desktop-wallpaper/265/998/desktop-wallpaper-girls-minimalism-pink-background-anime-mugs-school-group-anime-dp.jpg')]">
           Anime Girl To Email
         </h1>
+        <Image
+          src="/images/anya.jpeg"
+          width={500}
+          height={300}
+          alt="anya"
+          className="mb-5"
+        />
         <p>Enter your email to recieve an anime girl picture</p>
-        <form className="mt-5" onSubmit={handleSubmit}>
+        <form className="mt-5 flex w-full max-w-lg" onSubmit={handleSubmit}>
           <input
             type="email"
+            id="input-email"
             placeholder="yourname@example.com"
-            className="p-3 rounded-md rounded-r-none border"
+            className="p-3 rounded-md rounded-r-none border w-full"
             onChange={(e) => setEmail(e.target.value)}
             required
           />
-          <button className="bg-blue-500 p-3 border border-blue-600 rounded-r-md text-white">
+          <button
+            id="btn-submit"
+            className="bg-blue-500 p-3 border border-blue-600 rounded-r-md text-white"
+          >
             Submit
           </button>
         </form>
@@ -98,7 +110,10 @@ export default function Home() {
                     </svg>
                     <span className="sr-only">Success</span>
                   </div>
-                  <p className="mb-4 text-lg text-center font-semibold text-gray-900">
+                  <p
+                    id="output-msg"
+                    className="mb-4 text-lg text-center font-semibold text-gray-900"
+                  >
                     Successfully sent your email. Check your email to see the
                     result.
                   </p>
@@ -117,7 +132,25 @@ export default function Home() {
           </div>
         )}
         <div className="flex mt-10">
-          <a href="https://github.com/Qu1etboy/anime-girl-to-email">GitHub</a>
+          <a
+            href="https://github.com/Qu1etboy/anime-girl-to-email"
+            className="flex items-center gap-2 hover:bg-gray-100 p-2 rounded-md"
+          >
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+                className="fill-black"
+              >
+                <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
+              </svg>
+            </span>
+            Source
+          </a>
         </div>
       </main>
     </>
